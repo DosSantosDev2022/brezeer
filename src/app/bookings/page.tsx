f600684dadc3d8e05f2535d2901f4d9a25045179
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-02-06 21:56:32 -0300

    feat: add bookingpage

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -1,5 +1,75 @@
-export default function BookingsPage() {
+import { Header } from "@/components/header";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/prisma";
+import { BookingItem } from "@/components/booking-item";
+
+
+export default async function BookingsPage() {
+  const session = await getServerSession(authOptions)
+
+  if(!session){
+    return redirect('/')
+  }
+
+  const [confirmedBookings, finishedBookings] = await Promise.all([
+    db.booking.findMany({
+      where:{
+        userId: (session.user as any).id,
+        date: {
+          gte: new Date(),
+        },
+      },
+      include: {
+        service: true,
+        barbershop: true
+      },
+    }),
+    db.booking.findMany({
+      where: {
+        userId: (session.user as  any).id,
+        date: {
+          lt: new Date(),
+        },
+      },
+      include:  {
+        service: true,
+        barbershop: true
+      }
+    })
+  ])
+
   return (
-    <h1>Página de agendamentos</h1>
+    <>
+      <Header/>
+
+      <div className="px-5 py-6">
+        <h1 className="text-xl font-bold mb-6">Agendamentos</h1>
+
+          {confirmedBookings.length > 0 && (
+            <>
+              <h2 className="text-gray-400 uppercase font-bold text-sm mb-3">Confirmados</h2>
+              <div className="flex flex-col gap-3">
+                {confirmedBookings.map((booking) => (
+                  <BookingItem key={booking.id} booking={booking}  />
+                ))}
+              </div>
+            </>
+          )}
+
+
+          {finishedBookings.length > 0 && (
+            <>
+              <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3">Finalizados</h2>
+              <div className="flex flex-col gap-3">
+                {finishedBookings.map((booking) => (
+                  <BookingItem key={booking.id} booking={booking}  />
+                ))}
+              </div>
+            </>
+          )}
+      </div>
+    </>
   )
 }
