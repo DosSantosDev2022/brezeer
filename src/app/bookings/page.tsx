59de4d4097c13584ded8f97f27f55bf7f19c35ff
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-02-07 23:55:58 -0300

    fix: remove header component

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -43,8 +43,6 @@ export default async function BookingsPage() {
 
   return (
     <>
-      <Header/>
-
       <div className="px-5 py-6">
         <h1 className="text-xl font-bold mb-6">Agendamentos</h1>
 
